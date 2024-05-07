8dd770eb069913dfd386f682b161706346f9fb49
José Martínez <marquevjosman@gmail.com>
2024-05-07 14:41:12 -0600

    up

--- fish/build.fish
+++ fish/build.fish
@@ -1,7 +1,7 @@
 #!/bin/fish
-fish/init.fish $argv
+fish/init.fish $argv[1]
 if not test $status -eq 0
     exit $status
 end
 set jobs 16
-cmake --build build --config $argv --parallel $jobs
+cmake --build build --config $argv[1] --target $argv[2] --parallel $jobs --
--- fish/run.fish
+++ fish/run.fish
@@ -3,4 +3,4 @@ fish/build.fish $argv
 if not test $status -eq 0
     exit $status
 end
-build/muza
+./build/$argv[2]
